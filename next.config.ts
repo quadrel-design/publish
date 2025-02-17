e866cb6e4d9aab222575dc6709117c3695ca8a41
Nick Oates <nick@nickoates.com>
2025-02-16 20:12:20 -0800

    Convert tabs to spaces

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	output: 'export',
-	basePath: process.env.GITHUB_REPO_NAME
-		? `/${process.env.GITHUB_REPO_NAME}`
-		: undefined,
+  output: 'export',
+  basePath: process.env.GITHUB_REPO_NAME
+    ? `/${process.env.GITHUB_REPO_NAME}`
+    : undefined,
 };
 
 export default nextConfig;
